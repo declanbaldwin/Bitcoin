0faff0c6f8fc0ac9d1e2a82e9d93cf1b0eb5953d
Declan Baldwin <dbaldwin4@boxfusionconsulting.com>
2018-12-18 19:26:59 +0000

    added Account button

--- server.js
+++ server.js
@@ -1,96 +1,138 @@
-require('./config/config');
+require("./config/config");
 
-const _ = require('lodash');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 const port = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken");
 
 const app = express();
-const { mongoose } = require('./db/mongoose');
-const { Post } = require('./models/posts');
-const { User } = require('./models/user');
-const {authenticate} = require('./middleware/authenticate');
-const cookieParser = require('cookie-parser');
+const { mongoose } = require("./db/mongoose");
+const { Post } = require("./models/posts");
+const { User } = require("./models/user");
+const { authenticate } = require("./middleware/authenticate");
+const cookieParser = require("cookie-parser");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    var {token} = req.cookies;
-    Post.find({}).then((posts) => {
-        res.render('index', {
-            posts: posts,
-            token: token
+app.get("/", (req, res) => {
+    var { token } = req.cookies;
+    var results = {};
+
+    if(!token) {
+        Post.find({}).then(posts => {
+            return res.render("index", {
+                posts: posts,
+                token: token,
+                user: null
+            });
         });
-    }).catch((error) => {
-        res.status(404).send(error);
-    });
+    }
+
+    Post.find({})
+        .then(posts => {
+            results.posts = posts;
+        })
+        .then(() => {
+            var decoded = jwt.verify(token, process.env.JWT_SECRET);
+            return User.findOne({
+                '_id': decoded._id,
+                'tokens.token': token,
+                'tokens.access': 'auth'
+            }).then(user => {
+                results.user = user;
+            });
+        })
+        .then(() => {
+            console.log(results);
+            return res.render("index", {
+                posts: results.posts,
+                token: token,
+                user: results.user
+            });
+        })
+        .catch(error => {
+            res.status(404).send(error);
+        });
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
-app.get('/logout', (req, res) => {
-    if(!req.token) {
-        return res.clearCookie('token').status(200).redirect('/');
+app.get("/logout", (req, res) => {
+    if (!req.token) {
+        return res
+            .clearCookie("token")
+            .status(200)
+            .redirect("/");
     }
     req.user.removeToken(req.token).then(() => {
-        return res.clearCookie('token').status(200).redirect('/');
-    }), () => {
-        res.status(400).send();
-    };
+        return res
+            .clearCookie("token")
+            .status(200)
+            .redirect("/");
+    }),
+        () => {
+            res.status(400).send();
+        };
 });
 
-app.get('/posts', (req, res) => {
-    Post.find({}).then((posts) => {
-        res.send({posts});
-    }, (e) => {
-        res.status(400).send(e);
-    });
+app.get("/posts", (req, res) => {
+    Post.find({}).then(
+        posts => {
+            res.send({ posts });
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
 });
 
-app.get('/posts/:id', (req, res) => {
+app.get("/posts/:id", (req, res) => {
     var id = req.params.id;
 
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(400).send();
     }
 
     Post.findOne({
-        _id:id
-    }).then((post) => {
-        if(!post) {
+        _id: id
+    }).then(post => {
+        if (!post) {
             return res.status(400).send();
-        } 
-        res.send({post});
+        }
+        res.send({ post });
     });
 });
 
-app.post('/posts', authenticate, (req, res) => {
-    
-    User.findById(req.user._id).then((user) => {
-        let post = new Post({
-            title: req.body.title,
-            postType: req.body.postType,
-            author: user.firstName,
-            body: req.body.text,
-            createdAt: new Date()
+app.post("/posts", authenticate, (req, res) => {
+    User.findById(req.user._id)
+        .then(user => {
+            let post = new Post({
+                title: req.body.title,
+                postType: req.body.postType,
+                author: user.firstName,
+                body: req.body.text,
+                createdAt: new Date()
+            });
+
+            return post.save();
+        })
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch(error => {
+            res.status(404).send(error);
         });
-
-        return post.save();
-    }).then(() => {
-        res.redirect('/');
-    }).catch((error) => {
-        res.status(404).send(error);
-    });
 });
 
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
     var user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -98,24 +140,31 @@ app.post('/users', (req, res) => {
         password: req.body.password
     });
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
-        res.status(200).cookie('token', token, {httpOnly: true}).redirect('/');
-    }).catch((e) => {
-        res.status(400).send(e);
-    });
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then(token => {
+            res.status(200)
+                .cookie("token", token, { httpOnly: true })
+                .redirect("/");
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        });
 });
 
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.cookie('token', token, {httpOnly: true}).redirect('/');
+app.post("/users/login", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"]);
+    User.findByCredentials(body.email, body.password)
+        .then(user => {
+            return user.generateAuthToken().then(token => {
+                res.cookie("token", token, { httpOnly: true }).redirect("/");
+            });
+        })
+        .catch(e => {
+            res.status(400).send();
         });
-    }).catch((e) => {
-        res.status(400).send(); 
-    });
 });
 
 app.listen(port, () => {
--- test/server.test.js
+++ test/server.test.js
@@ -9,10 +9,10 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 chai.should();
 
-beforeEach((done) => {
+beforeEach(done => {
     Post.remove({}).then(() => done());
 });
-beforeEach((done) => {
+beforeEach(done => {
     User.remove({}).then(() => done());
 });
 
@@ -41,32 +41,35 @@ beforeEach((done) => {
 // });
 
 describe("POST /users", () => {
-    it('should create a new user', done => {
-        var password = 'myPassword';
+    it("should create a new user", done => {
+        var password = "myPassword";
         chai.request(app)
-            .post('/users')
-            .type('form')
+            .post("/users")
+            .type("form")
             .send({
                 _method: "post",
-                firstName: 'myFirstName',
-                lastName: 'myLastName',
-                email: 'myemail@gmail.com',
+                firstName: "myFirstName",
+                lastName: "myLastName",
+                email: "myemail@gmail.com",
                 password: password
             })
             .end((err, res) => {
-                if(err) {
+                if (err) {
                     return done(err);
                 }
                 expect(res).to.have.status(200);
                 // expect(res).to.have.cookie('token');
 
-                User.find({email: 'myemail@gmail.com'}).then((user) => {
-                    expect(user).to.have.lengthOf(1);
-                    expect(user.password).to.not.equal(password);
-                    done();
-                }).catch((e) => {
-                    done(e);
-                });
+                User.find({ email: "myemail@gmail.com" })
+                    .then(user => {
+                        expect(user).to.have.lengthOf(1);
+                        expect(user.password).to.not.equal(password);
+                        done();
+                    })
+                    .catch(e => {
+                        done(e);
+                    });
             });
     });
+
 });
